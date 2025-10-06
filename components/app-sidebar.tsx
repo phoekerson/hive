66368909236b416a91b030cd32cbc1b48c7fea6d
Caleb Mintoumba <mintoumbacaleb@gmail.com>
2025-10-06 20:08:38 +0200

    sync the side-bar with the user profile

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import * as React from "react"
 import { GalleryVerticalEnd } from "lucide-react"
 
@@ -13,164 +14,169 @@ import {
   SidebarMenuSubButton,
   SidebarMenuSubItem,
 } from "@/components/ui/sidebar"
+import { Button } from "./ui/button"
+import { UserButton, useUser } from "@clerk/nextjs"
 
 // This is sample data.
-const data = {
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Community",
-      url: "#",
-      items: [
-        {
-          title: "Contribution Guide",
-          url: "#",
-        },
-      ],
-    },
-  ],
-}
+// const data = {
+//   navMain: [
+//     {
+//       title: "Getting Started",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Installation",
+//           url: "#",
+//         },
+//         {
+//           title: "Project Structure",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Building Your Application",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Routing",
+//           url: "#",
+//         },
+//         {
+//           title: "Data Fetching",
+//           url: "#",
+//           isActive: true,
+//         },
+//         {
+//           title: "Rendering",
+//           url: "#",
+//         },
+//         {
+//           title: "Caching",
+//           url: "#",
+//         },
+//         {
+//           title: "Styling",
+//           url: "#",
+//         },
+//         {
+//           title: "Optimizing",
+//           url: "#",
+//         },
+//         {
+//           title: "Configuring",
+//           url: "#",
+//         },
+//         {
+//           title: "Testing",
+//           url: "#",
+//         },
+//         {
+//           title: "Authentication",
+//           url: "#",
+//         },
+//         {
+//           title: "Deploying",
+//           url: "#",
+//         },
+//         {
+//           title: "Upgrading",
+//           url: "#",
+//         },
+//         {
+//           title: "Examples",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "API Reference",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Components",
+//           url: "#",
+//         },
+//         {
+//           title: "File Conventions",
+//           url: "#",
+//         },
+//         {
+//           title: "Functions",
+//           url: "#",
+//         },
+//         {
+//           title: "next.config.js Options",
+//           url: "#",
+//         },
+//         {
+//           title: "CLI",
+//           url: "#",
+//         },
+//         {
+//           title: "Edge Runtime",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Architecture",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Accessibility",
+//           url: "#",
+//         },
+//         {
+//           title: "Fast Refresh",
+//           url: "#",
+//         },
+//         {
+//           title: "Next.js Compiler",
+//           url: "#",
+//         },
+//         {
+//           title: "Supported Browsers",
+//           url: "#",
+//         },
+//         {
+//           title: "Turbopack",
+//           url: "#",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Community",
+//       url: "#",
+//       items: [
+//         {
+//           title: "Contribution Guide",
+//           url: "#",
+//         },
+//       ],
+//     },
+//   ],
+// }
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const {user} = useUser();
   return (
     <Sidebar variant="floating" {...props}>
       <SidebarHeader>
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-              <a href="#">
-                <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                  <GalleryVerticalEnd className="size-4" />
+              <div className="flex items-center justify-between w-full">
+                <div className="flex flex-col">
+                  <span className="text-xs text-muted-foreground">
+                    Welcome back
+                  </span>
+                  <span className="text-sm font-semibold">
+                    {user?.firstName} {user?.lastName}
+                  </span>
                 </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-medium">Documentation</span>
-                  <span className="">v1.0.0</span>
-                </div>
-              </a>
+                <UserButton signInUrl="/sign-in" />
+              </div>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
@@ -178,26 +184,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <SidebarGroup>
           <SidebarMenu className="gap-2">
-            {data.navMain.map((item) => (
-              <SidebarMenuItem key={item.title}>
-                <SidebarMenuButton asChild>
-                  <a href={item.url} className="font-medium">
-                    {item.title}
-                  </a>
-                </SidebarMenuButton>
-                {item.items?.length ? (
-                  <SidebarMenuSub className="ml-0 border-l-0 px-1.5">
-                    {item.items.map((item) => (
-                      <SidebarMenuSubItem key={item.title}>
-                        <SidebarMenuSubButton asChild isActive={item.isActive}>
-                          <a href={item.url}>{item.title}</a>
-                        </SidebarMenuSubButton>
-                      </SidebarMenuSubItem>
-                    ))}
-                  </SidebarMenuSub>
-                ) : null}
-              </SidebarMenuItem>
-            ))}
+            <Button className="w-full" variant="outline">
+              Start New Chat
+            </Button>
+            {/*  Channel list */}
           </SidebarMenu>
         </SidebarGroup>
       </SidebarContent>
